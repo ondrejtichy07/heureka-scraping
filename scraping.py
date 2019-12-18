6710610ca4e286da29422eb6ea2f66b2592203de
Ondřej <ondrejtichy@ondrejs-macbookpro.local>
2019-12-19 00:10:19 +0100

    second commit

--- scraping.py
+++ scraping.py
@@ -1,33 +1,44 @@
 #Python program to scrape website
 from bs4 import BeautifulSoup
-from openpyxl import Workbook
 from datetime import date
 from termcolor import colored
+import xlsxwriter
 import csv
 import requests
 import time
 
 URL = 'https://mysi.heureka.cz/logitech-mx-master-3-advanced-wireless-mouse-910-005710/'
-r = requests.get(URL)
-num = 1
 
-soup = BeautifulSoup(r.content, 'html5lib')
-value = soup.find('span', {'class': 'js-top-price'}).get_text()
-
-res = value.replace(' Kč','')
-result = res.replace(' ', '')
-print(colored(result,'green'))
+column_num = 1
+column_date = 0
+values = []
+dates = []
 
 def print_value():
+    r = requests.get(URL)
+    row = 0
+    soup = BeautifulSoup(r.content, 'html5lib')
+    value = soup.find('span', {'class': 'js-top-price'}).get_text()
+
     today = date.today().strftime("%d.%m.%Y")
-    name = "price_for_mx_master3.xlsx"
-    workbook = Workbook()
-    sheet = workbook.active
-    sheet[f'A{num}'] = today
-    sheet[f'B{num}'] = int(result)
-    workbook.save(filename=name)
-    time.sleep(86400)
+
+    res = value.replace(' Kč','')
+    result = res.replace(' ', '')
+    print(colored(result,'green'))
+
+    values.append(result)
+    dates.append(today)
+
+    workbook = xlsxwriter.Workbook('price_for_mx_master3.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    for item in values:
+        worksheet.write(row, column_num, item)
+        worksheet.write(row, column_date, today)
+        row += 1
+
+    workbook.close()
+    time.sleep(6)
 
 while True:
     print_value()
-    num+=1
